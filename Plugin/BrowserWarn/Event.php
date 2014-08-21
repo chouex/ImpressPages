37c7a7cc45f7294591ec7f2785d2098cd6613fdf
charles <charles@PC3250>
2014-08-21 17:23:05 +0800

    *ie warn once

--- Plugin/BrowserWarn/Event.php
+++ Plugin/BrowserWarn/Event.php
@@ -4,8 +4,10 @@ namespace Plugin\BrowserWarn;
 
 class Event {
 	public static function ipBeforeController() {
-
-		ipAddJs('assets/warn.js');
-		ipAddCss('assets/warn.css');
+		if (!array_key_exists('iewarn', $_COOKIE)) {
+			setcookie ( "iewarn", 1 );
+			ipAddJs ( 'assets/warn.js' );
+			ipAddCss ( 'assets/warn.css' );
+		}
 	}
 }
